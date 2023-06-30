c9f997be12d1caf7b2461a9e3f3b41a304ee338e
Rishitha2404 <137592583+Rishitha2404@users.noreply.github.com>
2023-06-29 19:56:06 -0500

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruit_choice= streamlit.text_input("What fruit would you like information about?",'kiwi')
-streamlit.fruit_choice("User selected: ", fruit_choice)
+streamlit.write("User selected: ", fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
