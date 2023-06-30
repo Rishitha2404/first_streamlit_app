95a5b0179d56678b14628301c0d50e48564d2425
Rishitha2404 <137592583+Rishitha2404@users.noreply.github.com>
2023-06-29 19:54:11 -0500

    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,10 @@ fruits_to_show = my_fruit_list. loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
+fruit_choice= streamlit.text_input("What fruit would you like information about?",'kiwi')
+streamlit.fruit_choice("User selected: ", fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
